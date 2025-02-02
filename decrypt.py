62ef27ed72d527c92e9f776f848dcf302b967404
dkamien66 <hakmin0506@gmail.com>
2025-02-02 15:27:03 -0600

    Added nltk library to tokenize sentences and check if each word is an english word. However I'm running into problems where an english word is not in the list of words from nltk.corpus.

--- decrypt.py
+++ decrypt.py
@@ -2,6 +2,11 @@
 # Try all 25 possible keys to decrypt the ciphertext, saving each result in a new string.
 # Look at all 25 resulting strings. Most of them should be gibberish. Do any of them make sense? Can you figure out which one was the correct key?
 
+import nltk
+from nltk.corpus import words
+from nltk import word_tokenize
+
+nltk.download('words')
 
 def main():
     with open("coded_message.txt","r") as file:
@@ -13,6 +18,8 @@ def brute_force_decrypt(message):
     characters = "abcdefghijklmnopqrstuvwxyz,.!?'()0123456789 "
 
     with open("brute_force_decryptions.txt","w") as new_file:
+        actual_decryption = ""
+        actual_found = True
         for alpha_increment in range(1, 44):
             possible_decryption = ""
             for i in range(len(message)):
@@ -20,5 +27,20 @@ def brute_force_decrypt(message):
                 new_index = (alphabet_index + alpha_increment) % 44
                 possible_decryption += characters[new_index]
             new_file.write(possible_decryption + "\n")
+            # split into words and check if all words with only letters are english words
+            list = word_tokenize(possible_decryption)
+            print(list)
+            for item in list:
+                if item.isalpha():
+                    if not is_english_word(item): # if any word with letters is not an english word, this decryption cannot be the actual one
+                        actual_found = False
+            print(actual_found)
+            if actual_found:
+                actual_decryption = possible_decryption
+        # print decryption message at bottom 
+        new_file.write(f"\nThe actual decryption was found to be \"{actual_decryption}\"")
+
+def is_english_word(word):
+    return word in words.words()
     
 main()
